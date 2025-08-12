cc87b416b4adaa6913fa5ca75403044c805999b2
YASHVI <yashvivanza01@gmail.com>
2025-08-12 21:37:41 +0530

    Initial commit

---
+++ [01] Introduction.py
@@ -0,0 +1,12 @@
+'''
+    Python is an interprested, Object-Oriented and gigh evel programming langauge.
+'''
+a = 2 + 3
+b = 9 - 8
+c = 4 * 6
+d = 8 / 4
+e = 5 // 2
+f = (8 + 2) * 3
+g = 2 ** 3
+h = 10 % 3
+print(a,b,c,d,e,f,g,h,sep="\n")
