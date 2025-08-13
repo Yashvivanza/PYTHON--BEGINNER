2eaf0a69519807504be20b4a05744a0b7b9be881
YASHVI <yashvivanza01@gmail.com>
2025-08-13 18:41:18 +0530

    Initial commit

---
+++ [02] String.py
@@ -0,0 +1,24 @@
+a = 'navin'
+print(a)
+
+print('navin')
+
+print("navin")
+
+# print('navin's a laptop') - error
+
+print("navin's  laptop")
+
+# print("navin "laptop"") - error
+
+print('navin "laptop"')
+
+print('navin\'s "laptop"')
+
+a = 'navin ' + 'navin'
+print(a)
+
+b = 10 * 'navin '
+print(b)
+
+print(r'c:\docs\navin')
