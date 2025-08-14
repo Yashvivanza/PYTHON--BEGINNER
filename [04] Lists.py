9342bc74de8e63671763f7c64176a65c6aed5c6b
YASHVI <yashvivanza01@gmail.com>
2025-08-14 23:18:24 +0530

    Initial commit

---
+++ [04] Lists.py
@@ -0,0 +1,55 @@
+nums = [25,12, 95]
+print(nums)
+print(nums[0])
+print(nums[1])
+print(nums[2])
+print(nums[1:])
+print(nums[-1])
+print(nums[-3:-1])
+print("")
+
+names=['navin','kiran','john']
+print(names)
+print("")
+
+values=[9.5,'Naivn',25]
+print(values)
+print("")
+
+nums = [25,12, 95]
+names=['navin','kiran','john']
+mil = [nums,names]
+print(mil)
+print("")
+
+nums = [25,12, 95]
+nums.append(45) #  we can write direct value for adding element
+print(nums)
+print("")
+
+nums.insert(2,23) # We declare index  while adding element
+print(nums)
+print("")
+
+nums.remove(95) # We can delete  direct value from the list
+print(nums)
+print("")
+
+nums.pop(1) # We declare index for deleting
+print(nums)
+print("")
+
+nums.pop() # Here it deletes last element
+print(nums)
+print("")
+
+del nums[1:] # how to delete multiply values
+print(nums)
+print("")
+
+nums.extend([29,12,14,36])
+print(nums)
+print("")
+
+nums.sort()
+print(nums)
