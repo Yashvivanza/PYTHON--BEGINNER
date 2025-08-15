da75a06b97719a00d112770e3b8af0b659021591
YASHVI <yashvivanza01@gmail.com>
2025-08-15 21:53:52 +0530

    Initial commit

---
+++ [05] Tuple.py
@@ -0,0 +1,14 @@
+# In tuple we cannot change value - it is immutable
+# Denoted by ()
+tup = (21,36,14,25,21)
+print(tup)
+print("")
+
+print(tup[1]) # When we want to fetch values we use [] brackets.
+print("")
+
+# tup[1] = 33  error
+
+n = tup.count(21)
+print(n)
+print("")
