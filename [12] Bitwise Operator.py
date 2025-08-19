3262a98366c93babe755e504ae18f41f126f83b4
YASHVI <yashvivanza01@gmail.com>
2025-08-19 22:16:05 +0530

    Initial commit

---
+++ [12] Bitwise Operator.py
@@ -0,0 +1,28 @@
+a = ~12
+print(a)
+print("")
+
+b = 12 & 13
+print(b)
+print("")
+
+c = 12 | 13
+print(c)
+print("")
+
+d = 25 & 30
+print(d)
+print("")
+
+e = 12 ^ 1
+print(e)
+print("")
+
+d = 12 ^ 13
+print(d)
+print("")
+
+e = 25 ^ 30
+print(e)
+print("")
+
