bddb69a0bacbbfc7cc509e136406cabab7dc8466
YASHVI <yashvivanza01@gmail.com>
2025-08-19 22:35:56 +0530

    Initial commit

---
+++ [13] Import Math functions.py
@@ -0,0 +1,33 @@
+import math 
+x = math.sqrt(15)
+print(x)
+print("")
+
+print(math.floor(2.9)) # lowest value
+print(math.ceil(2.2)) # Highest value
+print("")
+
+a = 3 ** 2
+print(a)
+print("")
+
+print(math.pow(3,2))
+print("")
+
+print(math.pi)
+print("")
+
+print(math.e)
+print("")
+
+import math as m
+print(math.sqrt(25))
+print("")
+
+import math as m
+print(m.sqrt(25))
+print("")
+
+from math import sqrt,pow
+print(pow(4,5))
+print("")
