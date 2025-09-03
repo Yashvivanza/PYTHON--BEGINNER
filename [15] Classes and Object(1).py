dcddd6aa7e2b3d1efbc21367fb653fd5fa6bcc96
YASHVI <yashvivanza01@gmail.com>
2025-09-03 18:27:05 +0530

    Initial commit

---
+++ [15] Classes and Object(1).py
@@ -0,0 +1,14 @@
+class Book:
+    def __init__(self, title, quantity, author, price):
+        self.title = title
+        self.quantity = quantity
+        self.author = author
+        self.price = price
+    def __repr__(self):
+        return f"Book: {self.title}, Quantity: {self.quantity}, Author: {self.author}, Price: {self.price}"
+book1 = Book('Book 1', 12, 'Author 1', 120)
+book2 = Book('Book 2', 18, 'Author 2', 220)
+book3 = Book('Book 3', 28, 'Author 3', 320)
+print(book1)
+print(book2)
+print(book3)
